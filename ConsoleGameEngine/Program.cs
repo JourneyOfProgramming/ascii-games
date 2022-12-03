554c0dd854f124c20dc907c23fd2c95d2f29e248
JourneyOfProgramming <journey.of.programming@gmail.com>
2022-12-03 17:24:39 +0100

    Episode 1 - Hello world in game engine

---
+++ ConsoleGameEngine/Program.cs
@@ -0,0 +1,16 @@
+﻿using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+InitWindow(800, 480, "Hello World");
+
+while (!WindowShouldClose())
+{
+    BeginDrawing();
+    ClearBackground(Color.WHITE);
+
+    DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+
+    EndDrawing();
+}
+
+CloseWindow();
